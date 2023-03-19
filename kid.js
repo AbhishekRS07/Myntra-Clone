7efaddd4a9c13896defe254d39db52067ddc6681
Dibakardebnath <ddibakar190@gmail.com>
2023-03-19 13:22:22 +0530

    created men women and kid page

---
+++ kid.js
@@ -0,0 +1,309 @@
+
+
+var productArrK=[
+    {
+        image_url:
+        "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQ4g0Vh9eVQuVOMQ-pqyez1CbVRyvNVATZOaBXVjueMWNu0o7woTE85doVOjL0uSAyOCyRtpCQ1_grfXbc9xsEeQyBshH60-ZZZoLPeQL5kszct38XVBkYARg&usqp=CAE",
+        name: "Baby's  T-shirt Pant",
+        price: 499,
+        strikedoffprice: 899,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRSZ-QbYbP-J8hxkCZx01vCkF7ECZQ60DGjPKibb4IJ60cTstOwZlW28JeBszTv2xpgZnCL1kFSdUtAnV4j1gQEKnDAKNufK70XPMtcLRypI1pYSDAi3_6l&usqp=CAE",
+        name: " Boy's Cotton Printed ",
+        price: 645,
+        strikedoffprice: 1099,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcR853lD0yvYlMFBw_LmBaP4qEPrZO1NR8--PlXf3TMxoIDnYvqVrEJnxSR7Eez8vYyC09gZsZVY8LM3cNbTJhrAhfMpypbe&usqp=CAE",
+        name: "Boys' Pack Of 3 T-shirts",
+        price: 599,
+        strikedoffprice: 999,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQBWC9CfjPPNX_mr_D_oJ-40njrfQ2kaRSOAfss5s_eCly9hPAu2y0ejVCnB-7qyVk_tOhou53IWEqcFvpBgMEJ-uoAQ5rTYGrfaLe34CrI-0DQdzTYZSQG&usqp=CAE",
+        name: "Boy's Regular Fit T-",
+        price: 620,
+        strikedoffprice: 2599,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQnwTGGrr-efKWNilvFlTxBue5zybFiNH14lqdq0jo0u7eZF1_VAhIVnrX2-YH8-kUAaanetIjT5lrjTfHQ5nXrLYjlZPFtc-ndHS0R8D9tA62_Nbd2xQqK&usqp=CAE",
+        name: "Bold N Elegant ",
+        price: 999,
+        strikedoffprice: 2999,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSxmOi_Q1ZRGDvlIa2n-IpwXTbhaZ5peD9jn8t82kvuT7F36Y7CnB3itnhj1zBz1vZlN4zY6EcRVjcSaLzeM6qVswaXysMQqrqwWXBLP8fPUvn74fDMAnDs&usqp=CAE",
+        name: "Cotton Fleece Clothing ",
+        price: 745,
+        strikedoffprice: 3099,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQ76LC93X9OsaOJhANx4Xmm111opWmTJaJk5dQEVOplJEUrKyR--ZkxJAnPuPmkFO-6SjNomX6Ymnsubq2IJIJg2HtRSMLpbraLScNH40aQ&usqp=CAE",
+        name: "Baby's  T-shirt Pant",
+        price: 499,
+        strikedoffprice: 899,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTqAu077qtQDf2t7kWwDJk_D8KM9rdAZpgd5grxUkBPn1gfsxk31lxNOdv-SOjnpg5L6w6l1Cpi9e28m_Wis5MysYq_NbrMQvSlTO7hkv4Bsvswo8bjbLmw&usqp=CAE",
+        name: " Boy's Cotton Printed ",
+        price: 645,
+        strikedoffprice: 1099,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQds3i2zlxhfygggAAii1HZLFxGlfROuVeBacrOpre5TsmBBsIwM9xK7SqT-5EYdyYHRgwUp0v9jZ8WxFcOpiKaGuW4OOHC9xgF9JwuEy-UUaJ-tqC5dl4b-Q&usqp=CAE",
+        name: "Boys' Pack Of 3 T-shirts",
+        price: 599,
+        strikedoffprice: 999,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQBWC9CfjPPNX_mr_D_oJ-40njrfQ2kaRSOAfss5s_eCly9hPAu2y0ejVCnB-7qyVk_tOhou53IWEqcFvpBgMEJ-uoAQ5rTYGrfaLe34CrI-0DQdzTYZSQG&usqp=CAE",
+        name: "Boy's Regular Fit T-",
+        price: 620,
+        strikedoffprice: 2599,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQnwTGGrr-efKWNilvFlTxBue5zybFiNH14lqdq0jo0u7eZF1_VAhIVnrX2-YH8-kUAaanetIjT5lrjTfHQ5nXrLYjlZPFtc-ndHS0R8D9tA62_Nbd2xQqK&usqp=CAE",
+        name: "Bold N Elegant ",
+        price: 999,
+        strikedoffprice: 2999,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSxmOi_Q1ZRGDvlIa2n-IpwXTbhaZ5peD9jn8t82kvuT7F36Y7CnB3itnhj1zBz1vZlN4zY6EcRVjcSaLzeM6qVswaXysMQqrqwWXBLP8fPUvn74fDMAnDs&usqp=CAE",
+        name: "Cotton Fleece Clothing ",
+        price: 745,
+        strikedoffprice: 3099,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQoVCPttG28Oexed4qoqx0LxkYFTTlDUHXWaVDY7tk-I0jJ8pE6eahdZ4Os5FRixNh8XUIp4zUHyQoX1crbyYYGHzvp1mo7fDGccx9cVIeA&usqp=CAE",
+        name: "Boy's Regular Fit T-",
+        price: 620,
+        strikedoffprice: 2599,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQFacuGjm-_ZhCYiD4BfG1Erb4QWXxsjdBwfxzMNDyVtnWLoufwQLVl0FUz-5hlAQYoCiCaqp0T9IYl-aw1CUdmlZ-A1WTD&usqp=CAE",
+        name: "Bold N Elegant ",
+        price: 999,
+        strikedoffprice: 2999,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTRsTGRONuEqhd2QrGrICSKlNztwIPXzweybOlq3xkAh4S_wvcz94vDQXDaTU8Inc2Af5Ye42_GpBdaeLz-ZajcBW3sa1ZS4nuSJUL1y_BprnrMq6oM9VaMcQ&usqp=CAE",
+        name: "Cotton Fleece Clothing ",
+        price: 745,
+        strikedoffprice: 3099,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSsq9YzxSxqFliPZ428M8_XtCqa7W5gUaaBoq1zy_p9C3nTNwsSRldyApiAYKrKSNjwg9LSykRFYIvGc0NQeDCqoQEKj8jMCCrurBfc6KJguA0bFVWV6UXL&usqp=CAE",
+        name: "Boys' Pack Of 3 T-shirts",
+        price: 599,
+        strikedoffprice: 999,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRBN-ZfStjXoktRPidqHMmk-Uk3uc37DW9Mn3JYzKn1rFPp9Kx5nzAUA3Oi7lin_Kq1sA3n3KMGvCYhWbOGcMOHWe3QwSOcaIKUTsPBJKFfsbeXZc89OCyIJg&usqp=CAE",
+        name: "Boy's Regular Fit T-",
+        price: 620,
+        strikedoffprice: 2599,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRKh8ivw_y0W5jzM1Uqm_H59bL3dpP_XTp-AHn62PxtJjEYrvH6EusB9k8i6L8JPzCPFCfVALpHYmf6MLYLBG_DtcQv-EfkYIvU2OvVJ4nTYMCZwR7K4IaV&usqp=CAE",
+        name: "Bold N Elegant ",
+        price: 999,
+        strikedoffprice: 2999,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQHXtRjvgiLBRMIZGI9WgEKLFew1U0tX943t-MZGe9L4NPGnY9a2JS6ftKQq0Wqd5RCKErSGxJ41mb00PfUcznrCcrfGJIMF8K2L_Tgsko&usqp=CAE",
+        name: "Boy's Regular Fit T-",
+        price: 620,
+        strikedoffprice: 2599,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTRP9GcP5CZ2hVlXkmqahJoTxhYSLst2YcGCIbhWAW0mxCNXKbRH7qHkAOeA5gGPcXHa64VAvItzSum2RlhKNVGrHbUGOSrKAoqYHJz4mzZJgK9VgDxvSof&usqp=CAE",
+        name: "Bold N Elegant ",
+        price: 999,
+        strikedoffprice: 2999,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRiNF5kn_q0UJUfrF3z_HoGdHA5AmX5neUMT-kwt6ZBJ3_xlsnnV5w2zGb8Uu4Q5GzVWbXva_UDqa8KaT_Q-D1WpHMoKcuskllPGsOF2xY&usqp=CAE",
+        name: "Cotton Fleece Clothing ",
+        price: 745,
+        strikedoffprice: 3099,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQ4g0Vh9eVQuVOMQ-pqyez1CbVRyvNVATZOaBXVjueMWNu0o7woTE85doVOjL0uSAyOCyRtpCQ1_grfXbc9xsEeQyBshH60-ZZZoLPeQL5kszct38XVBkYARg&usqp=CAE",
+        name: "Baby's  T-shirt Pant",
+        price: 499,
+        strikedoffprice: 899,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRSZ-QbYbP-J8hxkCZx01vCkF7ECZQ60DGjPKibb4IJ60cTstOwZlW28JeBszTv2xpgZnCL1kFSdUtAnV4j1gQEKnDAKNufK70XPMtcLRypI1pYSDAi3_6l&usqp=CAE",
+        name: " Boy's Cotton Printed ",
+        price: 645,
+        strikedoffprice: 1099,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcR853lD0yvYlMFBw_LmBaP4qEPrZO1NR8--PlXf3TMxoIDnYvqVrEJnxSR7Eez8vYyC09gZsZVY8LM3cNbTJhrAhfMpypbe&usqp=CAE",
+        name: "Boys' Pack Of 3 T-shirts",
+        price: 599,
+        strikedoffprice: 999,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQBWC9CfjPPNX_mr_D_oJ-40njrfQ2kaRSOAfss5s_eCly9hPAu2y0ejVCnB-7qyVk_tOhou53IWEqcFvpBgMEJ-uoAQ5rTYGrfaLe34CrI-0DQdzTYZSQG&usqp=CAE",
+        name: "Boy's Regular Fit T-",
+        price: 620,
+        strikedoffprice: 2599,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQnwTGGrr-efKWNilvFlTxBue5zybFiNH14lqdq0jo0u7eZF1_VAhIVnrX2-YH8-kUAaanetIjT5lrjTfHQ5nXrLYjlZPFtc-ndHS0R8D9tA62_Nbd2xQqK&usqp=CAE",
+        name: "Bold N Elegant ",
+        price: 999,
+        strikedoffprice: 2999,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSxmOi_Q1ZRGDvlIa2n-IpwXTbhaZ5peD9jn8t82kvuT7F36Y7CnB3itnhj1zBz1vZlN4zY6EcRVjcSaLzeM6qVswaXysMQqrqwWXBLP8fPUvn74fDMAnDs&usqp=CAE",
+        name: "Cotton Fleece Clothing ",
+        price: 745,
+        strikedoffprice: 3099,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQ76LC93X9OsaOJhANx4Xmm111opWmTJaJk5dQEVOplJEUrKyR--ZkxJAnPuPmkFO-6SjNomX6Ymnsubq2IJIJg2HtRSMLpbraLScNH40aQ&usqp=CAE",
+        name: "Baby's  T-shirt Pant",
+        price: 499,
+        strikedoffprice: 899,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTqAu077qtQDf2t7kWwDJk_D8KM9rdAZpgd5grxUkBPn1gfsxk31lxNOdv-SOjnpg5L6w6l1Cpi9e28m_Wis5MysYq_NbrMQvSlTO7hkv4Bsvswo8bjbLmw&usqp=CAE",
+        name: " Boy's Cotton Printed ",
+        price: 645,
+        strikedoffprice: 1099,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcR853lD0yvYlMFBw_LmBaP4qEPrZO1NR8--PlXf3TMxoIDnYvqVrEJnxSR7Eez8vYyC09gZsZVY8LM3cNbTJhrAhfMpypbe&usqp=CAE",
+        name: "Boys' Pack Of 3 T-shirts",
+        price: 599,
+        strikedoffprice: 999,
+    },
+    
+    
+];
+    displayDataK(productArrK);
+
+    function sortFunK(){
+      var select=document.getElementById("sortPriceK").value;
+      console.log(select)
+       if(select === "htl"){
+        productArrK = productArrK.sort(function(a,b){
+          return b.price - a.price;
+        })
+        displayDataK(productArrK);
+       }
+       if(select === "lth"){
+        productArrK = productArrK.sort(function(a,b){
+          return a.price - b.price;
+        })
+        displayDataK(productArrK);
+       }
+       if(select === "all"){
+       window.location.reload();
+       }
+    }
+
+    function displayDataK(productArrK){
+
+        document.getElementById("mainK").textContent="";
+
+       productArrK.map(function(elm){
+        
+          var divK=document.createElement("div");
+         
+  
+          var imageK=document.createElement("img");
+          imageK.setAttribute("id","imageK");
+          imageK.setAttribute("src",elm.image_url);
+          imageK.setAttribute("alt",elm.name);
+        //   imageK.addEventListener("click",function(){addProduct(elm)});
+
+          var h5K =document.createElement("h5");
+          h5K.textContent=elm.name;
+  
+          var priceBoxK=document.createElement("div");
+          priceBoxK.setAttribute("id","boxK");
+  
+          var priceK=document.createElement("p");
+          priceK.textContent="₹ "+elm.price
+  
+          var MRPK=document.createElement("p");
+          MRPK.textContent="₹ "+elm.strikedoffprice;
+  
+          var btnK =document.createElement("button");
+          btnK.textContent="Details"
+          btnK.setAttribute("id","btnK");
+        //   btnK.addEventListener("click",function(){addCartK(elm)});
+        btnK.addEventListener("click",function(){addProduct(elm)});
+          // appending of elements
+  
+            priceBoxK.append(priceK,MRPK);
+            divK.append(imageK,h5K,priceBoxK,btnK);
+            document.getElementById("mainK").append(divK);
+  
+       });
+     };
+     
+    //  var cartArrK=JSON.parse(localStorage.getItem("aadToCart3")) || [];
+  
+    //   function addCartK(elm){
+    //     cartArrK.push(elm);
+    //     localStorage.setItem("aadToCart3", JSON.stringify(cartArrK));
+    //     // window.location.reload();
+    //     window.location.replace("./kid.html");
+    //   }
+
+
+      var cart1K =JSON.parse(localStorage.getItem("aadToCart1"))||[];
+      var cart2K =JSON.parse(localStorage.getItem("aadToCart2"))||[];
+      var cart3K =JSON.parse(localStorage.getItem("aadToCart3"))||[];
+
+      var totalIK=cart1K.length+cart2K.length+cart3K.length;
+      document.querySelector("#itemK").textContent=totalIK;
+
+      document.getElementById("logoK").addEventListener("click",logoFunK);
+   function logoFunK(){
+    window.location.replace("./index.html")
+   };
+
+
+   var proArr =JSON.parse(localStorage.getItem("productKid"))||[];
+   function addProduct(elm){
+    proArr.push(elm);
+    localStorage.setItem("productKid", JSON.stringify(proArr)); 
+    window.location.replace("./kidDetail.html");
+
+   }
---
+++ women.js
@@ -0,0 +1,340 @@
+
+   var productArrW=[
+    {
+        image_url:
+        "https://rukminim1.flixcart.com/image/832/832/xif0q/dress/r/h/f/xs-f-53-micozy-original-imaggf6dpevpuvhh-bb.jpeg?q=70",
+        name: "NPLASH FASHION",
+        price: 799,
+        strikedoffprice: 899,
+        q:1,
+    },
+    {
+        image_url:
+        "https://img101.urbanic.com/v1/goods-pic/4bd196f285da406997e774e7ff4464c8_w1000_q90.webp",
+        name: " IB STYLES",
+        price: 1245,
+        strikedoffprice: 1509,
+        q:1,
+    },
+    {
+        image_url:
+        "https://img101.urbanic.com/v1/goods-pic/ba074681826e422784af0001d2f7c9d0UR_w1000_q90.webp",
+        name: "Impex Women's Tops",
+        price: 599,
+        strikedoffprice: 999,
+        q:1,
+    },
+    {
+        image_url:
+        "https://rukminim1.flixcart.com/image/832/832/xif0q/dress/l/u/s/l-5700-varnifashion-original-imagfh6478nhvm7f.jpeg?q=70",
+        name: "Women's Casual Puff Sleeves ",
+        price: 1220,
+        strikedoffprice: 2099,
+        q:1,
+    },
+    {
+        image_url:
+        "https://img101.urbanic.com/v1/goods-pic/c7a27347a17145cc8494c313a9b9ac18UR_w1000_q90.webp",
+        name: "Poly Viscose  Shirt",
+        price: 1699,
+        strikedoffprice: 3999,
+        q:1,
+    },
+    {
+        image_url:
+        "https://cdn.shopify.com/s/files/1/0879/3716/products/NOVA14JAN-SUJ00409copy_1024x1024.jpg?v=1675424760",
+        name: " WOMEN'S Nivia",
+        price: 1945,
+        strikedoffprice: 4099,
+        q:1,
+    },
+    {
+        image_url:
+        " https://rukminim1.flixcart.com/image/832/832/kxrvi4w0/kurta/1/l/d/m-crepe-silk-maroon-long-beautiful-printed-belt-kurtis-dresses-original-imaga5nkraqvre9z.jpeg?q=70",
+        name: " Women's Western T-Shirt",
+        price: 1440,
+        strikedoffprice: 3199,
+        q:1,
+    },
+    {
+        image_url:
+        "https://www.southindiaeshop.com/pub/media/catalog/product/cache/f3083ab5e46fa316edf25a7bed8c5432/k/u/ku-ma-1258-blue-st-1.jpg",
+        name: " WOMEN'S  WORKOUT PANTS ",
+        price: 1999,
+        strikedoffprice: 3199,
+        q:1,
+    },
+    {
+        image_url:
+        "https://images.meesho.com/images/products/10586333/8b939_512.jpg",
+        name: "Women's M3 Loose Fit Jeans  ",
+        price: 769,
+        strikedoffprice: 1099,
+        q:1,
+    },
+    {
+        image_url:
+        "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/71DUK7MHe5S._UL1350_.jpg",
+        name: "WOMEN'S REEBOK Track Pants  ",
+        price: 1299,
+        strikedoffprice: 1440,
+        q:1,
+    },
+    {
+        image_url:
+        "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/51LlA-rkAAS.jpg",
+        name: " WOMEN'S  ELITAGE PANTS ",
+        price: 1999,
+        strikedoffprice: 3199,
+        q:1,
+    },
+    {
+        image_url:
+        "https://www.bullionknot.com/product-img/Albeli-Long-Dress8-1661408183.jpg",
+        name: "Women's M3 Fit Jeans  ",
+        price: 769,
+        strikedoffprice: 1099,
+        q:1,
+    },
+    {
+        image_url:
+        "https://rukminim1.flixcart.com/image/832/832/xif0q/dress/r/h/f/xs-f-53-micozy-original-imaggf6dpevpuvhh-bb.jpeg?q=70",
+        name: "NPLASH FASHION",
+        price: 799,
+        strikedoffprice: 899,
+        q:1,
+    },
+    {
+        image_url:
+        "https://img101.urbanic.com/v1/goods-pic/4bd196f285da406997e774e7ff4464c8_w1000_q90.webp",
+        name: " IB STYLES",
+        price: 1245,
+        strikedoffprice: 1509,
+        q:1,
+    },
+    {
+        image_url:
+        "https://img101.urbanic.com/v1/goods-pic/ba074681826e422784af0001d2f7c9d0UR_w1000_q90.webp",
+        name: "Impex Women's Tops",
+        price: 599,
+        strikedoffprice: 999,
+        q:1,
+    },
+    {
+        image_url:
+        "https://assets.ajio.com/medias/sys_master/root/20220805/LUob/62ed4081aeb26921afcae560/-473Wx593H-410324081-112558-MODEL.jpg",
+        name: "Women's  Denim Boot ",
+        price: 699,
+        strikedoffprice: 999,
+        q:1,
+    },
+    {
+        image_url:
+        "https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/b/0/tr:w-480,/b04e2f0RLO2007_6.jpg?rnd=20200526195200",
+        name: "WOMEN'S REEBOK SHOES  ",
+        price: 1620,
+        strikedoffprice: 3599,
+        q:1,
+    },
+    {
+        image_url:
+        "https://cdn.shopify.com/s/files/1/0581/5065/0043/products/Tornado_1.jpg?v=1663673441&width=823 ",
+        name: "Happy Shoes for Women",
+        price: 1099,
+        strikedoffprice: 4999,
+        q:1,
+    },
+    {
+        image_url:
+        "https://www.jiomart.com/images/product/600x750/rvvzbt697t/roddick-shoes-stylish-black-boot-shoes-for-party-shoes-for-womens-product-images-rvvzbt697t-0-202208290054.jpg",
+        name: "WOMEN'S  Adidas SHOES  ",
+        price: 1320,
+        strikedoffprice: 1899,
+        q:1,
+    },
+    {
+        image_url:
+        "https://img.thewhitepole.com/resized/412X548/1229/sf524-blue-6360d9f91c66f.jpg",
+        name: "Women's Tame Denim Boot",
+        price: 699,
+        strikedoffprice: 999,
+        q:1,
+    },
+    {
+        image_url:
+        "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRmTJWseyw4Qn_wu--x7YR7buzWNLXtF_rkuvZ4-k7SRtYoz1kIO4CaFEG99FipfoZRqpX7U6wwRVU0wkiMtUII3pEBOhuje8RF5HqHeNXCoccj9oOEpN1057Y&usqp=CAE",
+        name: "WOMEN'S RUNNING  SHOES  ",
+        price: 1620,
+        strikedoffprice: 3599,
+        q:1,
+    },
+    {
+        image_url:
+        "https://rukminim1.flixcart.com/image/832/832/xif0q/dress/r/h/f/xs-f-53-micozy-original-imaggf6dpevpuvhh-bb.jpeg?q=70",
+        name: "NPLASH FASHION",
+        price: 799,
+        strikedoffprice: 899,
+        q:1,
+    },
+    {
+        image_url:
+        "https://img101.urbanic.com/v1/goods-pic/4bd196f285da406997e774e7ff4464c8_w1000_q90.webp",
+        name: " IB STYLES",
+        price: 1245,
+        strikedoffprice: 1509,
+        q:1,
+    },
+    {
+        image_url:
+        "https://img101.urbanic.com/v1/goods-pic/ba074681826e422784af0001d2f7c9d0UR_w1000_q90.webp",
+        name: "Impex Women's Tops",
+        price: 599,
+        strikedoffprice: 999,
+        q:1,
+    },
+    {
+        image_url:
+        "https://rukminim1.flixcart.com/image/832/832/xif0q/dress/l/u/s/l-5700-varnifashion-original-imagfh6478nhvm7f.jpeg?q=70",
+        name: "Women's Casual Puff Sleeves ",
+        price: 1220,
+        strikedoffprice: 2099,
+        q:1,
+    },
+    {
+        image_url:
+        "https://img101.urbanic.com/v1/goods-pic/c7a27347a17145cc8494c313a9b9ac18UR_w1000_q90.webp",
+        name: "Poly Viscose  Shirt",
+        price: 1699,
+        strikedoffprice: 3999,
+        q:1,
+    },
+    {
+        image_url:
+        "https://cdn.shopify.com/s/files/1/0879/3716/products/NOVA14JAN-SUJ00409copy_1024x1024.jpg?v=1675424760",
+        name: " WOMEN'S Nivia",
+        price: 1945,
+        strikedoffprice: 4099,
+        q:1,
+    },
+    {
+        image_url:
+        " https://rukminim1.flixcart.com/image/832/832/kxrvi4w0/kurta/1/l/d/m-crepe-silk-maroon-long-beautiful-printed-belt-kurtis-dresses-original-imaga5nkraqvre9z.jpeg?q=70",
+        name: " Women's Western T-Shirt",
+        price: 1440,
+        strikedoffprice: 3199,
+        q:1,
+    },
+    {
+        image_url:
+        "https://rukminim1.flixcart.com/image/832/832/xif0q/dress/r/h/f/xs-f-53-micozy-original-imaggf6dpevpuvhh-bb.jpeg?q=70",
+        name: "NPLASH FASHION",
+        price: 799,
+        strikedoffprice: 899,
+        q:1,
+    },
+    {
+        image_url:
+        "https://img101.urbanic.com/v1/goods-pic/4bd196f285da406997e774e7ff4464c8_w1000_q90.webp",
+        name: " IB STYLES",
+        price: 1245,
+        strikedoffprice: 1509,
+        q:1,
+    },
+   
+    
+];
+     
+
+    displayDataW(productArrW);
+
+    function sortFunW(){
+      var select=document.getElementById("sortPriceW").value;
+      console.log(select)
+       if(select === "htl"){
+        productArrW = productArrW.sort(function(a,b){
+          return b.price - a.price;
+        })
+        displayDataW(productArrW);
+       }
+       if(select === "lth"){
+        productArrW = productArrW.sort(function(a,b){
+          return a.price - b.price;
+        })
+        displayDataW(productArrW);
+       }
+       if(select === "all"){
+       window.location.reload();
+       }
+    }
+
+
+   function displayDataW(productArrW){
+       
+    document.getElementById("mainW").textContent="";
+
+     productArrW.map(function(elm){
+      
+        var divW=document.createElement("div");
+       
+
+        var imageW=document.createElement("img");
+        imageW.setAttribute("id","imageW");
+        imageW.setAttribute("src",elm.image_url);
+        imageW.setAttribute("alt",elm.name);
+        // imageW.addEventListener("click",function(){addProductW(elm)});
+        var h5W =document.createElement("h5");
+        h5W.textContent=elm.name;
+
+        var priceBoxW=document.createElement("div");
+        priceBoxW.setAttribute("id","boxW");
+
+        var priceW=document.createElement("p");
+        priceW.textContent="₹ "+elm.price
+
+        var MRPW=document.createElement("p");
+        MRPW.textContent="₹ "+elm.strikedoffprice;
+
+        var btnW =document.createElement("button");
+        btnW.textContent="Details"
+        btnW.setAttribute("id","btnW");
+        // btnW.addEventListener("click",function(){addCartW(elm)});
+        btnW.addEventListener("click",function(){addProductW(elm)});
+        // appending of elements
+
+          priceBoxW.append(priceW,MRPW);
+          divW.append(imageW,h5W,priceBoxW,btnW);
+          document.getElementById("mainW").append(divW);
+
+     });
+   };
+   
+//    var cartArrW=JSON.parse(localStorage.getItem("aadToCart1")) || [];
+
+//     function addCartW(elm){
+//       cartArrW.push(elm);
+//       localStorage.setItem("aadToCart1", JSON.stringify(cartArrW));
+//       window.location.reload();
+//     }
+
+
+    var cart1W =JSON.parse(localStorage.getItem("aadToCart1"))||[];
+      var cart2W =JSON.parse(localStorage.getItem("aadToCart2"))||[];
+      var cart3W =JSON.parse(localStorage.getItem("aadToCart3"))||[];
+
+      var totalIW=cart1W.length+cart2W.length+cart3W.length;
+      document.querySelector("#itemW").textContent=totalIW;
+   
+      document.getElementById("logoW").addEventListener("click",logoFunW);
+   function logoFunW(){
+    window.location.replace("./index.html")
+   };
+
+
+   var proArrW =JSON.parse(localStorage.getItem("productWomen"))||[];
+   function addProductW(elm){
+    proArrW.push(elm);
+    localStorage.setItem("productWomen", JSON.stringify(proArrW)); 
+    window.location.replace("./womenDetail.html");
+
+   }
+      
